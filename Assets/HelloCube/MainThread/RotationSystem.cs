a3267bfb5d638588a9ef115bd1d6b7c71ce4ff15
nico233 <1601550204@qq.com>
2023-12-04 19:24:36 +0800

    添加HelloCube示例

---
+++ Assets/HelloCube/MainThread/RotationSystem.cs
@@ -0,0 +1,28 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace HelloCube.MainThread
+{
+    public partial struct RotationSystem : ISystem 
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<RotationSpeed>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            float deltaTime = SystemAPI.Time.DeltaTime;
+
+            // Ref 包含引用, RW 只写， RO 只读
+            foreach (var (transform, speed) in 
+                     SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotationSpeed>>())
+            {
+                transform.ValueRW = transform.ValueRO.RotateY(speed.ValueRO.RadiansPerSecond * deltaTime);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# dots_demo
+
+Unity [官方示例](https://github.com/Unity-Technologies/EntityComponentSystemSamples)
+的一些学习记录的代码
+
+环境：
++ Unity 2022.3.13 LTS
+
+笔记和代码详解更新于：[个人博客](http://nico233.cn/)
